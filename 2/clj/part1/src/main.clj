7b9474e33bb5cdfb063b6a5351ef00802b5deadd
Tim Janssen <tim@timjanssen.ca>
2025-12-02 08:27:18 -0500

    Day 2 part 1 solution

---
+++ 2/clj/part1/src/main.clj
@@ -0,0 +1,39 @@
+(ns main
+  (:require [clojure.string :as str]))
+
+(defn read-file
+  [filename]
+  (-> (slurp filename)
+      (str/split #"[\,\n]")))
+
+(defn read-range
+  [the-range]
+  (let [[low high] (str/split the-range #"\-")]
+    [(parse-long low) (parse-long high)]))
+
+(defn is-invalid-id
+  [id]
+  (boolean (re-matches #"^([0-9]+)\1$" (str id))))
+
+(defn invalid-ids-in-range
+  [[low high]]
+  (filter is-invalid-id (range low (+ high 1))))
+
+(->> (read-file "input.txt")
+     (map read-range)
+     (map invalid-ids-in-range)
+     (apply concat)
+     (reduce + 0))
+
+
+
+(comment
+
+  (is-invalid-id "123123")
+  (is-invalid-id "123123123")
+
+  (filter is-invalid-id (range 11 (+ 22 1)))
+
+  '())
+
+
