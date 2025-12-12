974f106840298a132f4988cf4b0e396bc1ed247c
Tim Janssen <tim@timjanssen.ca>
2025-12-12 10:11:46 -0500

    Day 11 part 1 solution

---
+++ clj/day11/part1/src/main.clj
@@ -0,0 +1,48 @@
+(ns main
+  (:require [clojure.string :as str]
+            [clojure.test :refer [#_#_with-test is run-tests]]))
+
+(defn read-file
+  [filename]
+  (-> (slurp filename)
+      (str/split-lines)))
+
+(defn read-links
+  [link-string]
+  (let [[node target-string] (str/split link-string #": ")]
+    [(keyword node) (map keyword (str/split target-string #" "))]))
+
+(defn calc-paths
+  [link-map node memo]
+  (if (= :out node)
+    [1 memo]
+    (if (contains? memo node)
+      [(node memo) memo]
+      (loop [num-paths 0
+             out-links    (link-map node)
+             memo         memo]
+        (if (empty? out-links)
+          [num-paths memo]
+          (let [[this-num-paths this-memo] (calc-paths link-map (first out-links) memo)]
+            (recur (+ num-paths this-num-paths) (rest out-links) (merge memo this-memo))))))))
+
+(let [link-map (->> (read-file "input.txt")
+                    (map read-links)
+                    (into {}))]
+  (first (calc-paths link-map :you {})))
+
+
+
+
+
+(comment
+
+
+
+
+
+  ;; Test
+  (run-tests)
+
+  '())
+
